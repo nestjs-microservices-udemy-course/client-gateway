05e32928c27116795249000cf2111a3ad1952ea1
Hair Zabala <hair.zabala@gmail.com>
2024-07-14 19:07:23 +1000

    feat: envs and pino logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,20 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
+import { prettyTarget } from './utils/pretty.target';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot({
+      pinoHttp: {
+        autoLogging: true,
+        customAttributeKeys: {
+          req: 'request',
+          res: 'response',
+          err: 'error',
+        },
+        transport: { target: prettyTarget },
+      },
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import envs from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = app.get(PinoLogger);
+
+  await app.listen(envs.PORT);
+  logger.log(`Gateway running on port ${envs.PORT}`);
 }
 bootstrap();
